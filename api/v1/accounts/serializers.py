923e9d75d16c06025310203a9faea108067b2337
akhilraj-c <aakhilraj265@gmail.com>
2023-08-03 00:55:17 +0530

    user api

--- api/v1/accounts/serializers.py
+++ api/v1/accounts/serializers.py
@@ -11,12 +11,4 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
-class UserSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    username = serializers.CharField()
-    first_name = serializers.CharField()
-    last_name = serializers.CharField()
-    role = serializers.CharField()
-    email = serializers.CharField()
-    department = serializers.CharField()
-    user_active = serializers.BooleanField()
+
