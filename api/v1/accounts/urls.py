f33e7df6db41acac1e1de8cfe595b9ffdd1481e8
MOHAMMED ASHIQ ALI K <ashiq5566.k@gmail.com>
2023-08-07 10:39:50 +0530

    small update

--- api/v1/accounts/urls.py
+++ api/v1/accounts/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path, re_path
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from .views import  *
 urlpatterns = [
-    #student_side
+    #user_side
     re_path(r"^login/$", login),
     re_path(r"^register/$", user_register),
     re_path(r"^users/$", user_list),
+
+
+    #toke generation using jwt
+    re_path(r"^token/$", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    re_path(r"^token/refresh/$", TokenRefreshView.as_view(), name="token_refresh"),
 ]
