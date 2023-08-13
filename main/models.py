86410aa8ef60c6b1815ae125d364400d4aefccdd
akhilraj9590 <aakhilraj265@gmail.com>
2023-08-13 14:45:22 +0530

    student semester model

--- main/models.py
+++ main/models.py
@@ -267,3 +267,18 @@ class Attendence(models.Model):
 
     def __str__(self):
         return f"{self.date}-{self.training_participant}"
+    
+class Student_semester(models.Model):
+    STATUS_CHOICES = (
+        ('completed', 'Completed'),
+        ('ongoing', 'Ongoing'),
+        ('upcoming', 'Upcoming'),
+    )
+    student = models.ForeignKey(Student,on_delete=models.CASCADE)
+    semester = models.ForeignKey(Program_Semester,on_delete=models.CASCADE)
+    start_date = models.DateField() 
+    end_date = models.DateField() 
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return f"{self.student},{self.semester},{self.status}"
